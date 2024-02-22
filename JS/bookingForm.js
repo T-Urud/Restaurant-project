26ece55b142f28a31e357d6fbc7a276a86b25be1
urud.theo@gmail.com <urud.theo@gmail.com>
2024-02-22 14:23:00 +0100

    css problems solved, JS on form

--- JS/bookingForm.js
+++ JS/bookingForm.js
@@ -1,6 +1,27 @@
 const today = new Date().toISOString().split("T")[0];
 booking_date.value = today;
 booking_date.min = today;
-
 const hour = new Date().toISOString();
-console.log(hour);
+
+const form = document.querySelector("form");
+
+function validPhone(phone) {
+  let regex = new RegExp("[0-9-+]");
+  if (!regex.test(phone)) {
+    throw new Error("le téléphone n'est pas valide");
+  }
+}
+
+const emailChecker = (value) => {
+  if (!value.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/i)) {
+    errorDisplay("email", "le mail n'est pas valide");
+    email = null;
+  } else {
+    errorDisplay("email", "", true);
+    email = value;
+  }
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
